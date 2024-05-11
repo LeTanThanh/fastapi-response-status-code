1f9e8ac07830a352048ce3da67c1e69e77b92608
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-11 21:37:08 +0700

    Shortcut to remember the names

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import status
 
 from typing import Annotated
 
-from http import HTTPStatus
-
 app = FastAPI()
-
+"""
 @app.post("/items", response_model=dict[str, str], status_code = HTTPStatus.CREATED.value)
+async def create_item(name: Annotated[str, Body(embed = True)]) -> dict:
+  return {"name": name}
+"""
+
+# Shortcut to remember the names
+@app.post("/items", response_model = dict[str, str], status_code = status.HTTP_201_CREATED)
 async def create_item(name: Annotated[str, Body(embed = True)]) -> dict:
   return {"name": name}
